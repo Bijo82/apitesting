b732a1a1efd454a5e38072c04fe0b0251b8e3ced
Bijo82 <bijosalu810@gmail.com>
2026-04-14 23:33:42 +0530

    inital commit

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route('/')
+def msg():
+    return "Hello"
+
+@app.route('/freq',methods=["POST"])
+def freq():
+    data = request.json.get("data")
+    if not data or "data" not in data:
+        return {'error':'Invalid input'},400
+    frequ={}
+    sdata = list(set(data))
+    for i in sdata:
+        count = data.count(i)
+        frequ.update({i:count})
+
+    return frequ
+
+if __name__=="__main__":
+    app.run()
